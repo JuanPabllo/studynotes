90f768e86630dd3f1d48e21ffb4fd1022dd54aaa
juanPabllo <juanpablo192.dreaw@gmail.com>
2021-04-24 16:55:13 -0300

    tuplas em py

---
+++ cursos/python/Estrutura-de-dados-em-python/tuplas.py
@@ -0,0 +1,9 @@
+# As tuplas, são estruturas de dados imutáveis(não podem ter seus dados alterados) envolvidas por parênteses ( ).
+
+dados = (3.14, "He4rt developers")
+
+# dados.append(75)
+
+# dados.remove(3.14)
+
+# dados[0] = -5
