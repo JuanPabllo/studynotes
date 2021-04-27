efc786d7f2925ba590954520e646b1d7c1c5a290
juanpabllo <juanpablo192.dreaw@gmail.com>
2021-04-27 20:11:34 -0300

    if else com py

---
+++ cursos/python/Estruturas-de-condição/if_else.py
@@ -0,0 +1,32 @@
+idade = 18
+
+if idade < 50:
+  print("Você é jovem!")
+elif idade > 50:
+  print("Você é idoso(a)!")
+else:
+  print("Idade inválida!")
+
+valor_de_entrada = 10
+
+if valor_de_entrada == 1:
+  print("A entreda era 1")
+elif valor_de_entrada == 2:
+  print("A entreda era 2")
+elif valor_de_entrada == 3:
+  print("A entreda era 3")
+elif valor_de_entrada == 4:
+  print("A entreda era 4")
+else:
+  print("O valor de entrada não era esperado em um if")
+
+
+a = 1
+
+if a == 1:
+  print("é 1")
+elif a >= 1:
+  print("é maior ou igual a 1")
+else:
+  print("é qualquer outra coisa")
+
