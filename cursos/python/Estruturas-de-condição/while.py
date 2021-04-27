5dc087f38ee3aae98c4939e7ccabd8c5d728f603
juanpabllo <juanpablo192.dreaw@gmail.com>
2021-04-27 20:11:40 -0300

    while com py

---
+++ cursos/python/Estruturas-de-condição/while.py
@@ -0,0 +1,20 @@
+i = 1
+# while i < 6:
+#     print(i)
+#     if i == 3:
+#         break
+#     i += 1
+
+while i < 6:
+    i += 1
+    if i == 3:
+        continue
+    print(i)
+
+# Else
+
+while i < 6:
+    print(i)
+    i += 1
+else:
+    print("i não é menor que 6")
